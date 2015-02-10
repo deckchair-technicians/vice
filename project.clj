1605f049e5bdef055e7adcb239493314bac63c27
David Williams <david.williams@callisto.com>
2015-02-10 12:23:10 +0000

    Minor: remove un-used lein plugins

--- project.clj
+++ project.clj
@@ -4,9 +4,6 @@
 
   :url "http://github.com/savagematt/vice"
 
-  :plugins [[s3-wagon-private "1.1.2"]
-            [lein-set-version "0.3.0" ]]
-
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-time "0.6.0"]
                  [prismatic/schema "0.2.5"]]
